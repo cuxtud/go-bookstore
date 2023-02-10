64a9244b26aa8e882828cf2eb63c2b760c381543
Anish <cuxtud@gmail.com>
2023-02-10 14:13:39 +0000

    updated

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "anish:Anish@123/simplerest?")
+	d, err := gorm.Open("mysql", "anish:Anish@123/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
